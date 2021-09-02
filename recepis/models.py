58b04b3c3bc192d5408a0df797a3953aabe2ec12
allyson.cz@hotmail.com <Aoa3698741@_>
2021-09-02 15:15:32 -0300

    finalizando o site com todos os testes realizados e bem execultados

--- recepis/models.py
+++ recepis/models.py
@@ -2,11 +2,14 @@ from django.db import models
 from datetime import datetime
 
 
-class Receitas(models.Model):
+class Receita(models.Model):
     nome = models.CharField(max_length=100)
     ingredientes = models.TextField()
     modo_preparo = models.TextField()
-    tempo_preparo = models.IntegerField()
+    tempo_preparo = models.CharField(max_length=50)
     rendimento = models.CharField(max_length=100)
     categoria = models.CharField(max_length=100)
     data_receita = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.nome
